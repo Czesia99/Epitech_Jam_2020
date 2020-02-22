21618704f8d1745bc6bb75713fea77d4b57090d0
Balthazard <balthazard.grego@epitech.eu>
2020-02-22 14:45:57 +0100

    On left click Spawn obj added

---
+++ Space_Fight_Studio/Assets/SandBox/Scripts/mySpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mySpawn : MonoBehaviour
+{
+    public GameObject ballPrefab;
+
+    public void onSpawn(Vector3 position)
+    {
+        Instantiate(ballPrefab).transform.position = position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
+            Vector3 adjustZ = new Vector3(worldPoint.x, worldPoint.y, ballPrefab.transform.position.z);
+
+            onSpawn(adjustZ);
+        }
+    }
+}
