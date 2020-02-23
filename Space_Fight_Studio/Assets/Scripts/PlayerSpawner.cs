ffe0b2e33e51d4666d87140ba95bbf904aa998f9
yann <yann.berger@epitech.eu>
2020-02-23 18:35:00 +0100

    player controllers

---
+++ Space_Fight_Studio/Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public GameObject player1;
+    public GameObject player2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player1.activeSelf == false) {
+            if (Input.GetButtonDown("P1.Start"))
+                player1.SetActive(true);
+        }
+        if (player2.activeSelf == false) {
+            if (Input.GetButtonDown("P2.Start"))
+                player2.SetActive(true);
+        }
+    }
+}
---
+++ Space_Fight_Studio/Assets/Scripts/Players/Player2Movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Movement : MonoBehaviour
+{
+    public PlayerController controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
+    bool jump = false;
+    bool dash = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("P2.Horizontal") * runSpeed;
+        verticalMove = Input.GetAxisRaw("P2.Vertical") * runSpeed;
+
+        if (Input.GetButtonDown("P2.Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("P2.Dash"))
+        {
+            dash = true;
+        }
+    }
+
+    void FixedUpdate() 
+    {
+        // controller.Move(horizontalMove * Time.fixedDeltaTime, jump, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump, dash, verticalMove);
+        jump = false;
+        dash = false;
+    }
+}
