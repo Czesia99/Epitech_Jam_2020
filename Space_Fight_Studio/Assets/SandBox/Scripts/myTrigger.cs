613c2b7c118f16fe59f5dbae977d854ae6ef9587
Balthazard <balthazard.grego@epitech.eu>
2020-02-23 04:50:13 +0100

    Add basic spawner w/ triggers

--- Space_Fight_Studio/Assets/SandBox/Scripts/myTrigger.cs
+++ Space_Fight_Studio/Assets/SandBox/Scripts/myTrigger.cs
@@ -5,24 +5,28 @@ using UnityEngine;
 public class myTrigger : MonoBehaviour
 {
     // Start is called before the first frame update
-    // public void onSpawn(Vector3 position)
-    // {
-    //     Instantiate(ballPrefab).transform.position = position;
-    // }
-    void Start()
+    private LevelManager myScript;
+    public bool Spawn = false;
+    void Awake()
     {
-        // rb = GetComponent<Rigidbody>();
+        myScript = gameObject.GetComponent<LevelManager>();
+
     }
+    // void Start()
+    // {
+    //     myScript = gameObject.GetComponent<LevelManager>();
+    // }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Player") {
-            Debug.Log("TESSST");
+            myScript.Spawn = true;
+
             // other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(10, 10, 0));
         }
     }
---
+++ Space_Fight_Studio/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject[] mygameObjects;
+    public bool stopSpawning = false;
+    public bool Spawn = false;
+    public float spawnTime;
+    public float spawnDelay;
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+    // void Awake()
+    // {
+    // }
+
+    public void SpawnObject() {
+        float arrayLen = mygameObjects.Length;
+        int i = (int)Random.Range(0f, arrayLen);
+        Instantiate(mygameObjects[i], transform.position, transform.rotation);
+        if (stopSpawning)
+            CancelInvoke("SpawnObject");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Spawn) {
+            InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+            Spawn = false;
+        }
+        
+    }
+}
