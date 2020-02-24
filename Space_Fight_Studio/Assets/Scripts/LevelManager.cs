7e586fad6e96c70aca454a273ab5202ee1b372a2
Balthazard <balthazard.grego@epitech.eu>
2020-02-24 01:04:02 +0100

    Advanced Spawner Added

--- Space_Fight_Studio/Assets/Scripts/LevelManager.cs
+++ Space_Fight_Studio/Assets/Scripts/LevelManager.cs
@@ -4,34 +4,80 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    public GameObject[] mygameObjects;
-    public bool stopSpawning = false;
-    public bool Spawn = false;
+    public GameObject[] myGameobject;
+    public bool onInstantiateStart = false;
+    public float Duration;
+    private float _duration = 0;
+    public bool Trigger = false;
+    private bool Active = false;
     public float spawnTime;
     public float spawnDelay;
+    /// <summary>
+    /// Change Properties of Objects Generated
+    ///
+    /// </summary>
+    public Vector2 spawnForce;
+    public bool incrementX;
+    public float incrementXBy = 10;
+    public bool incrementY;
+    public float incrementYBy = 10;
+    public float gravityScale = 1f;
+    public Transform position;
     // Start is called before the first frame update
     // void Start()
     // {
         
     // }
-    // void Awake()
-    // {
-    // }
+    void Awake()
+    {
+        if (onInstantiateStart)
+            setActive(true);
+    }
+    public bool isActive()
+    {
+        return (Active);
+    }
+    public void setActive(bool active)
+    {
+        if (active && !isActive()) {
+            Active = active;
+            InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+        }
+        else if (!active) {
+            Active = active;
+        }
 
+    }
     public void SpawnObject() {
-        float arrayLen = mygameObjects.Length;
+        float arrayLen = myGameobject.Length;
         int i = (int)Random.Range(0f, arrayLen);
-        Instantiate(mygameObjects[i], transform.position, transform.rotation);
-        if (stopSpawning)
+        GameObject JunkInstance;
+    
+        JunkInstance = Instantiate(myGameobject[i]);
+        JunkInstance.transform.SetParent(position, false);
+        // JunkInstance.transform = position;
+        JunkInstance.GetComponent<Rigidbody2D>().gravityScale = gravityScale;
+        JunkInstance.GetComponent<Rigidbody2D>().AddForce(spawnForce);
+        if (incrementX && Mathf.Abs(spawnForce.x) <= 1000)
+            spawnForce.x += incrementXBy;
+        if (incrementY &&  Mathf.Abs(spawnForce.y) <= 1000)
+            spawnForce.y += incrementYBy;
+        if (!isActive())
             CancelInvoke("SpawnObject");
     }
     // Update is called once per frame
     void Update()
     {
-        if (Spawn) {
-            InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
-            Spawn = false;
+        if (Trigger) {
+            setActive(true);
+            Trigger = false;
         }
-        
+        if (isActive()) {
+            Duration -= Time.deltaTime;
+            if (Duration <= 0) {
+                setActive(false);
+                Duration = _duration;
+            }
+        }
     }
 }
---
+++ Space_Fight_Studio/Assets/Scripts/Spawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mySpawn : MonoBehaviour
+{
+    public GameObject ballPrefab;
+
+    public void onSpawn(Vector3 position)
+    {
+        GameObject JunkInstance;
+        JunkInstance =  Instantiate(ballPrefab);
+        JunkInstance.transform.position = position;
+        JunkInstance.GetComponent<Rigidbody2D>().AddForce(new Vector2(10f * 100, 10f * 100));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
+            Vector3 adjustZ = new Vector3(worldPoint.x, worldPoint.y, ballPrefab.transform.position.z);
+
+            onSpawn(adjustZ);
+        }
+    }
+}
